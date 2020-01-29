7a930c8837c61cbaca81ad8e06e9d7dcc269dcd5
abin <abin2abraham@yahoo.co.in>
2020-01-29 11:54:04 +0530

    updated webpack config for css modules

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -434,6 +434,8 @@ module.exports = function(webpackEnv) {
               exclude: cssModuleRegex,
               use: getStyleLoaders({
                 importLoaders: 1,
+                modules: true,
+                localIdentName: '[name]__[local]__[hase:base64:5]',
                 sourceMap: isEnvProduction && shouldUseSourceMap,
               }),
               // Don't consider CSS imports dead code even if the
